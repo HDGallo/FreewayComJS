36a61fe0f7b3442ab2f94e7d6c541e14327855cf
HDGallo <92938974+HDGallo@users.noreply.github.com>
2022-09-26 17:36:14 -0300

    Create imagens.js

---
+++ imagens.js
@@ -0,0 +1,25 @@
+//variais do ator
+
+let imagemDaEstrada;
+let imagemAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+let somTrilha;
+let somColidiu;
+let somPontos;
+
+
+// carregamento das imagens e sons 
+function preload(){
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png");
+  imagemCarro2 = loadImage("imagens/carro-2.png");
+  imagemCarro3 = loadImage("imagens/carro-3.png");
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
+  somTrilha = loadSound("sons/trilha.mp3");
+  somColidiu = loadSound("sons/colidiu.mp3");
+  somPontos = loadSound("sons/pontos.wav")
+  
+}
